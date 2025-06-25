11ea6a6f7e6dd65cfae63f7312358de134dc2954
iliyan-velichkov <velichkov.iliyan@gmail.com>
2025-06-25 15:57:42 +0300

    update demo.sql

--- sales/db/demo.sql
+++ sales/db/demo.sql
@@ -6,19 +6,28 @@
 
 
 ------ 1. Preparation ------
+-- Delete all data from the cubes
 DELETE FROM TD_IS_C;
+DELETE FROM TD_RS_C;
 
 
 
 ------ 2. Check initial state ------
--- Expected X entries in TD_IS
-SELECT COUNT(*) from TD_IS; 
+-- Expected 60398 entries in DS_IS_TD
+SELECT COUNT(*) from DS_IS_TD; 
 
--- Show first 10 entries in TD_IS
-SELECT * from TD_IS LIMIT 10;
+-- Show first 10 entries in DS_IS_TD
+SELECT * from DS_IS_TD LIMIT 10;
 
--- Expected 0 entries in TD_IS_C
+-- Expected 60855 entries in DS_RS_TD
+SELECT COUNT(*) from DS_RS_TD; 
+
+-- Show first 10 entries in DS_RS_TD
+SELECT * from DS_RS_TD LIMIT 10;
+
+-- Expected 0 entries in cube tables
 SELECT COUNT(*) from TD_IS_C; 
+SELECT COUNT(*) from TD_RS_C; 
 
 
 
@@ -34,3 +43,11 @@ SELECT COUNT(*) from TD_IS_C;
 
 -- Show first 10 entries in TD_IS_C
 SELECT * from TD_IS_C LIMIT 10;
+
+
+-- Expected X entries in TD_RS_C
+SELECT COUNT(*) from TD_RS_C; 
+
+-- Show first 10 entries in TD_RS_C
+SELECT * from TD_RS_C LIMIT 10;
+
